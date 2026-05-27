0d71c470c5b7749f47d9ff07431fa62fa47d59f0
adekmzrk <6rxzgrx@gmail.com>
2026-05-27 15:46:50 +0700

    feat: register service worker on app load

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -3,6 +3,7 @@
 import { SessionProvider } from 'next-auth/react'
 import { Toaster } from 'sonner'
 import { ThemeProvider, useTheme } from './theme-provider'
+import { SwRegister } from './sw-register'
 
 function ThemedToaster() {
   const { resolvedTheme } = useTheme()
@@ -22,6 +23,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider>
       <SessionProvider>
+        <SwRegister />
         {children}
         <ThemedToaster />
       </SessionProvider>
